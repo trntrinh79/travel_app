21c428a94419ad8702643649d5bb4be76a47b79d
DESKTOP-M7UVLDP\Trinh <trntrinh79@gmail.com>
2023-06-14 17:04:46 +0700

    3

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:travel_app/cubit/app_cubit.dart';
+import 'package:travel_app/cubit/app_cubit_logics.dart';
+import 'package:travel_app/pages/detail_page.dart';
 import 'package:travel_app/pages/navpages/main_page.dart';
 import 'package:travel_app/pages/welcome_page.dart';
+import 'package:travel_app/services/data_services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +23,12 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MainPage(),
+      home: BlocProvider<AppCubits>(
+        create: (context) => AppCubits(
+          data: DataServices(),
+        ),
+        child: AppCubitLogics(),
+      ),
     );
   }
 }
--- lib/widgets/responsive_button.dart
+++ lib/widgets/responsive_button.dart
@@ -1,27 +1,42 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter/widgets.dart';
 import 'package:travel_app/misc/colors.dart';
+import 'package:travel_app/widgets/app_text.dart';
 
 class ResponsiveButton extends StatelessWidget {
   bool? isResponsive;
   double? width;
-  ResponsiveButton({super.key, this.width, this.isResponsive = false});
+  ResponsiveButton({super.key, this.width = 120, this.isResponsive = false});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: width,
-      height: 60,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: AppColors.mainColor,
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset("img/button-one.png"),
-        ],
+    return Flexible(
+      child: Container(
+        width: isResponsive == true ? double.maxFinite : width,
+        height: 60,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: AppColors.mainColor,
+        ),
+        child: Row(
+          mainAxisAlignment: isResponsive == true
+              ? MainAxisAlignment.spaceBetween
+              : MainAxisAlignment.center,
+          children: [
+            isResponsive == true
+                ? Container(
+                    margin: EdgeInsets.only(left: 20),
+                    child: AppText(
+                      text: "Book Trip Now",
+                      color: Colors.white,
+                    ),
+                  )
+                : Container(),
+            Image.asset("img/button-one.png"),
+          ],
+        ),
       ),
     );
   }
